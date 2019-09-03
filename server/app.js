0c0cf571d7c45efb75f54f98a9429b3e1f99827d
hule <hule@yiban.cn>
2019-09-03 11:02:35 +0800

    添加模拟的后端页面

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-02 16:46:57
- * @LastEditTime: 2019-09-02 18:16:30
+ * @LastEditTime: 2019-09-03 10:58:13
  * @LastEditors: Please set LastEditors
  */
 
@@ -17,10 +17,10 @@ const router = new Router();
 
 let config = process.env.NODE_ENV == 'production' ? {
   view: path.join(__dirname, './views'),
-  static: path.join(__dirname, '../app/src')
+  static: path.join(__dirname, '../app')
 } : {
   view: path.join(__dirname, './views'),
-  static: path.join(__dirname, '../app/src')
+  static: path.join(__dirname, '../app')
 };
 
 app.use(views(config.view, { extension: 'pug' }));
@@ -28,6 +28,14 @@ app.use(static(config.static));
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', { title: '首页' });
+});
+
+router.get('/about', async (ctx, next) => {
+  await ctx.render('about', { title: '关于' });
+});
+
+router.get('/other', async (ctx, next) => {
+  await ctx.render('other', { title: '其它' });
 });
 
 app.use(router.routes());
